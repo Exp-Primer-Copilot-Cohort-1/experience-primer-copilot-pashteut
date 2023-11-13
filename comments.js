4f3738e5f17842b90b9fd7ce5ede4782c0d05b5b
Babin Pavel Evgenyevich <75827878+pashteut@users.noreply.github.com>
2023-11-13 15:03:25 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,28 @@
+// Create web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Create event
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
+
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+        // Send event to event bus
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: { ...data, status }
+        });
+    }
+
+    res.send({});
+});
+
+app.listen(4003, () => {
+    console.log('Listening on 4003');
+});
